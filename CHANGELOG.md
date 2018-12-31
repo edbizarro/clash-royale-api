3d89db4e3afb7b2293bb0c68c4ceafc987ba0baf
Eduardo Bizarro <edbizarro@gmail.com>
2018-12-30 23:37:39 -0200

    Player

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,16 +2,23 @@
 
 All notable changes to `clash-royale-api` will be documented in this file
 
-## 0.2 - 2018-12-30
+## v0.2 - 2018-12-30
+
+### Clan
 
 - Clan Members
 - Clan War Log
 - Clan Current War
 
-## 0.1 - 2018-10-22
+### Player
+
+- Player info
+- Player battle log
+
+## v0.1 - 2018-10-22
 
 - Clan info and search
 
-## 0.0.1 - 2018-10-21
+## v0.0.1 - 2018-10-21
 
 - initial release
---
+++ src/Player.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Edbizarro\ClashRoyale;
+
+use Exception;
+use Tightenco\Collect\Support\Collection;
+
+class Player extends Resource
+{
+    protected $resourceName = 'players';
+
+    /**
+     * @param array $options
+     *
+     * @return Collection
+     * @throws Exception
+     *
+     * @see https://developer.clashroyale.com/api-docs/index.html#!/players/getPlayerBattles
+     */
+    public function battles(array $options = []): Collection
+    {
+        return $this->response(
+            $this->api->makeRequest(
+                $this->getResourceFinalUrl().'/battlelog',
+                $options
+            )
+        );
+    }
+}
