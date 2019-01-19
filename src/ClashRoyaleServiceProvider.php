ffaacfbad0898b863e2d3e6b38978054f605b067
Eduardo Bizarro <edbizarro@gmail.com>
2019-01-18 22:06:51 -0200

    fix provider

--- src/ClashRoyaleServiceProvider.php
+++ src/ClashRoyaleServiceProvider.php
@@ -7,13 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class ClashRoyaleServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
      * Bootstrap the application services.
      */
     public function boot()
