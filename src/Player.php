646c00858b9d580a64e9501f204a9853400cc868
Eric Sanders <easander@easander.com>
2020-05-05 06:00:00 -0500

    Add support for player upcoming chests (#14)
    * Updated README for new function and updated sample code for Player functions
    
    * Added new function to get the upcoming chests for a Player
    
    * Added punctuation to descriptions
    
    * Removed superfluous newline

--- src/Player.php
+++ src/Player.php
@@ -10,6 +10,8 @@ class Player extends Resource
     protected $resourceName = 'players';
 
     /**
+     * Returns a list of battles that the Player has recently participated in.
+     *
      * @param array $options
      *
      * @return Collection
@@ -22,6 +24,26 @@ class Player extends Resource
         return $this->response(
             $this->api->makeRequest(
                 $this->getResourceFinalUrl().'/battlelog',
+                $options
+            )
+        );
+    }
+
+    /**
+     * Returns the chests that are upcoming for the Player.
+     *
+     * @param array $options
+     *
+     * @return Collection
+     * @throws Exception
+     *
+     * @see https://developer.clashroyale.com/api-docs/index.html#!/players/getPlayerUpcomingChests
+     */
+    public function upcomingChests(array $options = []): Collection
+    {
+        return $this->response(
+            $this->api->makeRequest(
+                $this->getResourceFinalUrl().'/upcomingchests',
                 $options
             )
         );
